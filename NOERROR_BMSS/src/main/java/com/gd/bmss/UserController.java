d731c9bcac91553683c1e2d7dfb773d95572b364
SoftGitter <cobynine2@gmail.com>
2023-09-13 20:28:45 +0900

    회원가입 개발 미완성 joinPage,joinPage2,UserController,footer,header작성
    JSP파일 생성시 템플릿 만들었으니 쓰면댐

--- NOERROR_BMSS/src/main/java/com/gd/bmss/UserController.java
+++ NOERROR_BMSS/src/main/java/com/gd/bmss/UserController.java
@@ -1,8 +1,28 @@
 package com.gd.bmss;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 회원관리 컨트롤러
+ * @author KDH
+ *
+ */
 @Controller
 public class UserController {
-
+	
+	/*
+	 * 약관동의 이동
+	 */
+	@RequestMapping(value = "join.do")
+	public String joinUser() {
+		
+		return "joinPage";
+	}
+	@RequestMapping(value = "join2.do")
+	public String joinGo() {
+		
+		return "joinPage2";
+	}
+	
 }
