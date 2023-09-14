7e53d67ede807358450fa2246eb86d83f041694c
GITHUBUSER7701 <anhongla@naver.com>
2023-09-14 17:48:25 +0900

    수정했습니다

--- NOERROR_BMSS/src/main/java/com/gd/bmss/service/IOderService.java
+++ NOERROR_BMSS/src/main/java/com/gd/bmss/service/IOderService.java
@@ -1,5 +1,10 @@
 package com.gd.bmss.service;
 
+import java.util.List;
+
 public interface IOderService {
+
+	
+
 
 }
--- NOERROR_BMSS/src/main/java/com/gd/bmss/service/OderServiceImpl.java
+++ NOERROR_BMSS/src/main/java/com/gd/bmss/service/OderServiceImpl.java
@@ -1,8 +1,12 @@
 package com.gd.bmss.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class OderServiceImpl implements IOderService {
+
+
 
 }
