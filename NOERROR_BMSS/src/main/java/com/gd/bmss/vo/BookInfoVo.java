ad0a676cf5ab7f62160754d446613608cec97b8a
catnine11 <catnine11@gmail.com>
2023-09-14 11:57:57 +0900

    VO 올립니다

--- NOERROR_BMSS/src/main/java/com/gd/bmss/vo/BookInfoVo.java
+++ NOERROR_BMSS/src/main/java/com/gd/bmss/vo/BookInfoVo.java
@@ -1,5 +1,45 @@
 package com.gd.bmss.vo;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 도서 상세정보 Vo
+ * 도서상세정보(BOOKINFO)테이블과 장르테이블(BOOK_GENRE)테이블 통합
+ * @author NGR
+ * @since 2023.09.14.
+ */
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class BookInfoVo {
+
+	private String book_code;
+	private String isbn;
+	private String title;
+	private String author;
+	private String genre_code;
+	private String publisher;
+	private String price;
+	private String publish_date;
+	private String thumbnail;
+
+	private String content;
+	private String intro;
+	private String review;
+	private String authorinfo;
+
+	private String genre_name;
+
+	private List<Book_StatusVo> bsVo;
 
 }
--- NOERROR_BMSS/src/main/java/com/gd/bmss/vo/BorrowVo.java
+++ NOERROR_BMSS/src/main/java/com/gd/bmss/vo/BorrowVo.java
@@ -1,5 +1,35 @@
 package com.gd.bmss.vo;
 
+import java.util.Date;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 대출(BOOK_BORROW)테이블의 Vo
+ * @author NGR
+ * @since 2023.09.14.
+ */
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class BorrowVo {
+	
+	private String borrow_seq    ;
+	private String borrow_title  ;
+	private Date start_date    ;
+	private Date return_date   ;
+	private String renew         ;
+	private String borrow_status ;
+	private String user_id       ;
+	private String book_seq      ;
 
 }
